c7482ffc284eca235c51e35d3a72cb8761b6527e
mzanella <zanna0150@gmail.com>
2018-09-04 09:41:17 +0200

    Fix Packet and Builder classes

--- src/connection/Packet.cpp
+++ src/connection/Packet.cpp
@@ -1,82 +1,95 @@
-/*
- * Packet.cpp
- *
- *  Created on: Aug 28, 2018
- *      Author: zanna
- */
-
 #include "Packet.h"
 
-namespace connectionmanager {
+namespace connection {
 
 Packet::Packet() {}
 
 Packet::~Packet() {
-	delete packet;
+    delete packet;
 }
 
-Packet::Builder::Builder() : payload(Packet::Builder::DEFAULT_PAYLOAD),
-		tcp_dest_port(Packet::Builder::DEFAULT_TCP_DEST_PORT),
-		tcp_source_port(Packet::Builder::DEFAULT_TCP_SOURCE_PORT),
-		ip_dest_address(Packet::Builder::DEFAULT_IP_DEST_ADDRESS),
-		ip_source_address(Packet::Builder::DEFAULT_IP_SOURCE_ADDRESS),
-		eth_dest_address(Packet::Builder::DEFAULT_ETH_DEST_ADDRESS),
-		eth_source_address(Packet::Builder::DEFAULT_ETH_SOURCE_ADDRESS){}
-
-Packet::Builder* Packet::Builder::withRawPDU(const std::string& payload) {
-	this->payload = payload;
-	return this;
+std::vector<uint8_t> Packet::serialize() {
+    return packet->serialize();
 }
 
-Packet::Builder* Packet::Builder::withTCPDestinationPort(const int& dport) {
-	this->tcp_dest_port = dport;
-	return this;
+const std::string Packet::Builder::DEFAULT_PAYLOAD = "";
+const int Packet::Builder::DEFAULT_TCP_DEST_PORT = -1;
+const int Packet::Builder::DEFAULT_TCP_SOURCE_PORT = -1;
+const std::string Packet::Builder::DEFAULT_IP_DEST_ADDRESS = "-1";
+const std::string Packet::Builder::DEFAULT_IP_SOURCE_ADDRESS = "-1";
+const std::string Packet::Builder::DEFAULT_ETH_DEST_ADDRESS = "-1";
+const std::string Packet::Builder::DEFAULT_ETH_SOURCE_ADDRESS = "-1";
+
+Packet::Builder::Builder() : payload(""),
+        tcp_dest_port(-1),
+        tcp_source_port(-1),
+        ip_dest_address(""),
+        ip_source_address(""),
+        eth_dest_address(""),
+        eth_source_address(""){}
+
+Packet::Builder& Packet::Builder::with_raw_PDU(const std::string& payload) {
+    this->payload = payload;
+    return *this;
 }
 
-Packet::Builder* Packet::Builder::withTCPSourcePort(const int& sport) {
-	this->tcp_source_port = sport;
-	return this;
+Packet::Builder& Packet::Builder::with_TCP_destination_port(const int& dport) {
+    this->tcp_dest_port = dport;
+    return *this;
 }
 
-Packet::Builder* Packet::Builder::withIPDestinationAddress(std::string address) {
-	this->ip_dest_address = address;
-	return this;
+Packet::Builder& Packet::Builder::with_TCP_source_port(const int& sport) {
+    this->tcp_source_port = sport;
+    return *this;
 }
 
-Packet::Builder* Packet::Builder::withIPSourceAddress(std::string address) {
-	this->ip_source_address = address;
-	return this;
+Packet::Builder& Packet::Builder::with_IP_destination_address(std::string address) {
+    this->ip_dest_address = address;
+    return *this;
 }
 
-Packet::Builder* Packet::Builder::withEthernetDestinationAddress(std::string address) {
-	this->eth_dest_address = address;
-	return this;
+Packet::Builder& Packet::Builder::with_IP_source_address(std::string address) {
+    this->ip_source_address = address;
+    return *this;
 }
 
-Packet::Builder* Packet::Builder::withEthernetSourceAddress(std::string address) {
-	this->eth_source_address = address;
-	return this;
+Packet::Builder& Packet::Builder::with_ethernet_destination_address(std::string address) {
+    this->eth_dest_address = address;
+    return *this;
 }
 
-Packet Packet::Builder::build() {
-	Packet p;
-	p.packet = new Tins::RawPDU(payload);
-	if ((((this->eth_source_address).compare(DEFAULT_ETH_SOURCE_ADDRESS) ||
-			(this->eth_dest_address).compare(DEFAULT_ETH_DEST_ADDRESS))) != 0) {
-		Tins::EthernetII temp = Tins::EthernetII(this->eth_source_address, this->eth_dest_address);
-		p.packet = new Tins::EthernetII(temp / *p.packet);
-	}
-	if ((this->tcp_source_port != DEFAULT_TCP_SOURCE_PORT) &&
-			(this->tcp_dest_port != DEFAULT_TCP_DEST_PORT)) {
-		Tins::TCP temp = Tins::TCP(this->tcp_source_port, this->tcp_dest_port);
-		p.packet = new Tins::TCP(temp / *p.packet);
-	}
-	if ((((this->ip_source_address).compare(DEFAULT_IP_SOURCE_ADDRESS) ||
-			(this->ip_dest_address).compare(DEFAULT_IP_DEST_ADDRESS))) != 0) {
-		Tins::IP temp = Tins::IP(this->ip_source_address, this->ip_dest_address);
-		p.packet = new Tins::IP(temp / *p.packet);
-	}
-	return p;
+Packet::Builder& Packet::Builder::with_ethernet_source_address(std::string address) {
+    this->eth_source_address = address;
+    return *this;
+}
+
+Packet Packet::Builder::build() const{
+    Packet p;
+    p.packet = new Tins::RawPDU(payload);
+    if ((((this->eth_source_address).compare(DEFAULT_ETH_SOURCE_ADDRESS) ||
+            (this->eth_dest_address).compare(DEFAULT_ETH_DEST_ADDRESS))) != 0) {
+        Tins::EthernetII temp = Tins::EthernetII(this->eth_source_address, this->eth_dest_address);
+        Tins::PDU* to_delete = p.packet;
+        p.packet = new Tins::EthernetII(temp / *p.packet);
+        delete to_delete;
+    }
+    if ((this->tcp_source_port != DEFAULT_TCP_SOURCE_PORT) &&
+            (this->tcp_dest_port != DEFAULT_TCP_DEST_PORT)) {
+        Tins::TCP temp = Tins::TCP(this->tcp_source_port, this->tcp_dest_port);
+        Tins::PDU* to_delete = p.packet;
+        p.packet = new Tins::TCP(temp / *p.packet);
+        delete to_delete;
+    }
+    if ((((this->ip_source_address).compare(DEFAULT_IP_SOURCE_ADDRESS) ||
+            (this->ip_dest_address).compare(DEFAULT_IP_DEST_ADDRESS))) != 0) {
+        std::cout << this->ip_source_address << std::endl;
+        std::cout << this->ip_dest_address << std::endl;
+        Tins::IP temp = Tins::IP(this->ip_source_address, this->ip_dest_address);
+        Tins::PDU* to_delete = p.packet;
+        p.packet = new Tins::IP(temp / *p.packet);
+        delete to_delete;
+    }
+    return p;
 }
 
 } /* namespace connectionmanager */
